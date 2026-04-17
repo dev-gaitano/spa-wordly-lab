3e55894ce3d65bbb1a9485255a15d44328a57eb4
gaitano <dev.gaitano@gmail.com>
2026-04-17 16:52:44 +0300

    feat(js): added word data fetching and display logic
    Implemented fetchWordData to query dictionary API and displayData
    to extract word, phonetics, parts of speech, and definitions.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,53 @@
+// fetch word data
+async function fetchWordData(word) {
+  const res = await fetch(` https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+  const wordData = await res.json()
+
+  return wordData
+}
+
+// Display data
+function displayData(data) {
+  data.forEach(obj => {
+    const word = obj.word
+
+    // Pronunciation
+    const phonetic = obj.phonetic
+    const phonetics = obj.phonetics
+    // Pronunciation audio playback
+    let phoneticAudioAU = phonetics[0].audio
+    let phoneticAudioUS = phonetics[1].audio
+
+    // Definitions, partOfSpeech & synonyms
+    const meanings = obj.meanings
+    let partOfSpeech = meanings[0].partOfSpeech
+    const definitions = []
+    let synonyms = []
+    meanings.forEach(meaning => {
+      // Get definitions
+      meaning.definitions.forEach(definition => {
+        definitions.push(definition.definition)
+      })
+
+      // Get synonyms
+      synonyms.push(...meaning.synonyms)
+    });
+
+    console.log(word)
+    console.log(phonetic)
+    console.log(phoneticAudioAU)
+    console.log(phoneticAudioUS)
+    console.log(partOfSpeech)
+    console.log(definitions)
+    console.log(synonyms)
+  });
+}
+
+// Handle inputs and events
 // Search functionality
-// Display
-//  - Pronunciation
-//  - Definitions
-//  - Synonyms
-// Pronunciation audio playback
+async function init() {
+  const data = await fetchWordData("duck")
+  displayData(data)
+}
+
+init()
